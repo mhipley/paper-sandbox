a26e8f62fff80080126f104255c2344cc5631af1
Martha Hipley <info@marthahipley.com>
2020-01-07 12:48:47 -0600

    setting up SASS + simple local server to eliminate cross origin errors

---
+++ js/sandbox.js
@@ -0,0 +1,11 @@
+// Create a raster item using the image tag with id='mona'
+var raster = new Raster('sample');
+
+// Move the raster to the center of the view
+raster.position = view.center;
+
+// Scale the raster by 50%
+raster.scale(0.5);
+
+// Rotate the raster by 45 degrees:
+raster.rotate(45);
